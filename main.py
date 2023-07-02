7053cc5c0b49def104e24581b68f36efa5cabd86
Troy <101336233+tmgkopczyk@users.noreply.github.com>
2023-07-02 14:58:21 -0400

    created a function to return information of a weather station based on matching site name and province variables

--- main.py
+++ main.py
@@ -3,6 +3,23 @@ import requests
 import xmltodict
 from csv import DictReader
 
+# get_site_by_name returns a Python dict containing the data of a weather station if site name and province code variables match a station in a list
+def get_site_by_name(site_name, province):
+    body = requests.get("https://dd.weather.gc.ca/citypage_weather/docs/site_list_en.csv").text
+    sites = parse_site_csv(body)
+    # for each site in the Python list
+    for i in range(len(sites)):
+        site = sites[i]
+        # if the current site name and province code match what the user provided
+        if site["siteName"].strip().upper() == site_name.strip().upper() and site["province"].strip().upper() == province.strip().upper():
+            return site
+        # if it doesn't
+        else:
+            continue
+    # otherwise, return an error message
+    else:
+        return "No site with this name found."
+
 # parse_site_csv takes a CSV file and converts into a Python list
 def parse_site_csv(csv_file):
     sites = []
