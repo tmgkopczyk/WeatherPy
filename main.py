d835bb4ba94a734a22aa1b63bc8ce51f8de9cc4d
Troy <101336233+tmgkopczyk@users.noreply.github.com>
2024-08-14 09:01:02 -0400

    refactored existing code using external resources

--- main.py
+++ main.py
@@ -1,90 +1,142 @@
-# importing modules for use
-from dataclasses import dataclass
-from csv import DictReader
+from geopy.geocoders import Nominatim
 from requests import get
 from xmltodict import parse
+from csv import DictReader
+from geopy import distance
 
-@dataclass
-class WeatherStation:
-    code: str
-    siteName: str
-    province: str
-    longitude: float
-    latitude: float
+def get_user_coordinates(user_location: str):
+    geolocator = Nominatim(user_agent="tmgkopczyk@gmail.com")
+    user_coordinates = geolocator.geocode(user_location)
+    return user_coordinates.latitude, user_coordinates.longitude
 
-# get_weather_by_site_name returns the data of a weather station from the name that the user provides
 def get_weather_by_site_name(site_name: str, province: str):
-    # get the URL of the station using the site name and province provided
     url = get_site_url(site_name, province)
-    # return the XML data of the weather station using the URL provided as a Python dict
     return get_weather(url)
 
-# get weather_by_site_code returns the data of a weather station from the site code that the user provides
+def get_weather_by_user_location(user_location: str):
+    lat, lon = get_user_coordinates(user_location)
+    sites = get_ec_sites_list()
+    closest = closest_site(sites, lat, lon)
+    return get_weather_by_site_code(closest["Codes"], closest["Province Codes"])
+
 def get_weather_by_site_code(site_code: str, province: str):
-    # build the weather station URL using the provided variables
-    url = build_site_url(site_code,province)
-    # return the XML data of the weather station using the URL provided as a Python dict
+    url = build_site_url(site_code, province)
     return get_weather(url)
 
-# get_site_by_name parses the data of a station from a CSV file and returns it as a dataclass
 def get_site_by_name(site_name: str, province: str):
-    # make a GET request to Environment Canada's web server to retrieve the CSV file containing all the weather stations
-    body = get("https://dd.weather.gc.ca/citypage_weather/docs/site_list_en.csv")
-    # parse the CSV file into a Python list
-    sites = parse_site_csv(body.text)
-    # for each site in the list
-    for i in range(len(sites)):
-        site = sites[i]
-        # if the site name and province code match what is provided
-        if site.siteName.strip().upper() == site_name.strip().upper() and site.province.strip().upper() == province.strip().upper():
+    body = get("http://dd.weather.gc.ca/citypage_weather/xml/siteList.xml").text.encode("iso-8859-1").decode("utf-8")
+    sites = parse_site_xml(body)
+    for site in sites:
+        if site["siteName"].strip().upper() == site_name.strip().upper() and site["province"].strip().upper() == province.strip().upper():
             return site
         else:
             continue
-# get_weather returns the XML data located at the provided URL as a Python dict
-def get_weather(url: str):
-    # make a GET request to retrieve the XML data from the provided URL
-    body = get(url).content
-    # convert the XML file into a Python dict
+    return NameError("No site with this name found.")
+
+def get_weather(url):
+    body = get(url)
     return parse_weather_xml(body)
 
-# get_site_url builds and returns the URL of the page that hosts the requested XML data
 def get_site_url(site_name: str, province: str):
-    # get the dataclass corresponding to the site name and province provided
-    site = get_site_by_name(site_name,province)
-    # build the URL for the hosted XML data of the station
-    url = build_site_url(site.code, site.province)
-    # return the url
+    site = get_site_by_name(site_name, province)
+    url = build_site_url(site["code"], site["province"])
     return url
 
-# build_site_url returns the URL of the hosted XML data of the site using the provided site code and province
 def build_site_url(site_code: str, province: str):
     return f"http://dd.weather.gc.ca/citypage_weather/xml/{province}/{site_code}_e.xml"
 
-# parse_site_csv returns a Python list containing dataclasses for each weather station
-def parse_site_csv(csv_file):
-    # convert the CSV file into a Python-readable format
-    sites_reader = DictReader(csv_file.splitlines()[1:])
+def parse_site_xml(xml):
+    json = parse(xml)
     sites = []
-    # for each site in the CSV file
+    for site in json["siteList"]["site"]:
+        sites.append(
+            {
+                "siteName": site["nameEn"],
+                "code": site["@code"],
+                "province": site["provinceCode"]
+            }
+        )
+    return sites
+
+def parse_weather_xml(xml):
+    json = parse(xml.text)
+    weather_root = json["siteData"]
+    weather = {
+        "temperature": 'N/A',
+        "temperatureUnit": 'N/A',
+        "conditions": 'N/A',
+        "warnings": [],
+        "forecasts": [],
+        "relativeHumidityPercent": 0,
+        "windSummary": 'N/A',
+        "windChillSummary": 'N/A'
+    }
+
+    current_conditions = weather_root["currentConditions"]
+    if current_conditions:
+        temperature = current_conditions["temperature"]
+        weather["conditions"] = current_conditions["condition"]
+        weather["temperature"] = float(temperature["#text"])
+        weather["temperatureUnit"] = temperature["@units"]
+
+    if weather_root["forecastGroup"].get("forecast"):
+        for forecast in weather_root["forecastGroup"]["forecast"]:
+            pop = forecast["abbreviatedForecast"]["pop"]
+            weather["forecasts"].append({
+                "period": forecast["period"]["#text"],
+                "temperature": forecast["temperatures"]["textSummary"],
+                "pop": f"{pop['#text'] if pop.get('#text') else 0}{pop['@units']}",
+                "popSummary": forecast["precipitation"]["textSummary"],
+                "cloudPopSummary": forecast["cloudPrecip"]["textSummary"],
+                "relativeHumidityPercent":f'{forecast["relativeHumidity"]["#text"]}{forecast["relativeHumidity"]["@units"]}',
+                "windSummary": forecast["winds"].get("textSummary") if forecast.get("winds") and forecast["winds"].get("textSummary") else "N/A",
+                "windChillSummary": forecast["windChill"].get("textSummary") if forecast.get("windChill") and forecast["winds"].get("textSummary") else "N/A",
+                "summary": forecast["abbreviatedForecast"]["textSummary"],
+                "fullSummary": forecast["textSummary"]
+            })
+        try:
+            for warning in weather_root["warnings"]:
+                if weather_root["warnings"][warning]:
+                    try:
+                        weather["warnings"].append({
+                            "description": weather_root["warnings"][warning]["@description"],
+                            "priority": weather_root["warnings"][warning]["@priority"]
+                        })
+                    except TypeError:
+                        continue
+                else:
+                    continue
+        except TypeError:
+            pass
+    return weather
+
+def get_ec_sites_list():
+    sites = []
+    response = get("https://dd.weather.gc.ca/citypage_weather/docs/site_list_en.csv")
+    sites_csv_string = response.text.encode("iso-8859-1").decode("utf-8")
+    sites_reader = DictReader(sites_csv_string.splitlines()[1:])
     for site in sites_reader:
-        # if the province code is not HEF
         if site["Province Codes"] != "HEF":
-            # append the data of the weather station as a dataclass
-            sites.append(WeatherStation(
-                code=site["Codes"],
-                siteName = site["English Names"],
-                province = site["Province Codes"],
-                longitude = -1 * float(site["Longitude"][:-1]),
-                latitude = float(site["Latitude"][:-1])
-            ))
-        # otherwise, continue
+            site["Latitude"] = float(site["Latitude"][:-1])
+            site["Longitude"] = -1 * float(site["Longitude"][:-1])
+            sites.append(site)
         else:
             continue
     return sites
 
-def parse_weather_xml(xml):
-    weather_root = parse(xml)
-    return weather_root
 
-weather_data = get_weather_by_site_name("Ottawa (Richmond - Metcalfe)","ON")
-print(weather_data)
+def closest_site(site_list, lat, lon):
+    def site_distance(site):
+        return distance.distance((lat, lon), (site["Latitude"], site["Longitude"]))
+
+    closest = min(site_list, key=site_distance)
+    return closest
+
+
+def main():
+    weather_by_location = get_weather_by_user_location("14 Wiltshire Circle, Ottawa, ON")
+    print(weather_by_location["forecasts"][0]["fullSummary"])
+
+
+if __name__ == "__main__":
+    main()
